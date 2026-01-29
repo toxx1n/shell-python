9f9c700246c1519b56fbb785db2502f865b61b03
toxx1n <adithyaranjith11a@gmail.com>
2026-01-29 21:23:04 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import sys
 
 def main():
     
+    builtin = ['exit','echo','type']
     while True:
         cmd = input('$ ').strip()
 
@@ -10,6 +11,12 @@ def main():
             break
         elif cmd[:4] == 'echo':
             print(cmd[5:])
+        elif cmd[:4] == 'type':
+            command_to_check = cmd[5:]
+            if command_to_check in builtin:
+                print(f"{command_to_check} is a shell builtin")
+            else:
+                print(f"{command_to_check} not found")
         else:
             print(f"{cmd}: command not found")
 
