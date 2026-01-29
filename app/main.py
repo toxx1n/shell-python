a9fc299520e234215a1f693a218edcb90b1005e0
toxx1n <adithyaranjith11a@gmail.com>
2026-01-29 21:02:11 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def main():
     
     while True:
         cmd = input('$ ')
+        if cmd.strip() == 'exit':
+            break
         print(f"{cmd}: command not found")
 
 if __name__ == "__main__":
